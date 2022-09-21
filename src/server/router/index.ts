0a4a68cb20105c14a3170c5be2d28e301f3f4ada
Julian <85513960+JLN13X@users.noreply.github.com>
2022-09-22 00:05:50 +0200

    add prettier, husky, lintstaged

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,12 +3,10 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
+  .merge("example.", exampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
