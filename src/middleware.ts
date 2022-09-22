2ada0129e30fe15d2b9dd82e90fd7e398c9a3fde
Julian <85513960+JLN13X@users.noreply.github.com>
2022-09-22 21:30:02 +0200

    fix middleware matchers and add redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname;
-  if (!pathname.startsWith("/h")) return NextResponse.next();
-
-  const slug = pathname.split("/").pop();
+  const slug = req.nextUrl.pathname.split("/").pop();
 
   // Get slug from backend by fetching because prisma cant run on the edge
   const response = await fetch(
     `${req.nextUrl.origin}/api/url-by-slug?slug=${slug}`
   );
-
-  console.log({
-    ori: req.nextUrl.origin,
-  });
 
   if (!response.ok) return NextResponse.redirect(req.nextUrl.origin);
 
@@ -23,3 +16,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(data.url);
   }
 }
+
+export const config = {
+  matcher: ["/h/:path*"],
+};
--- src/pages/generate-link.tsx
+++ src/pages/generate-link.tsx
@@ -1,8 +1,26 @@
 import { GenerateLinkForm } from "@/components/generate-link-form";
-import type { NextPage } from "next";
+import { getServerAuthSession } from "@/server/common/get-server-auth-session";
+import type { GetServerSideProps, NextPage } from "next";
 
 const GenerateLink: NextPage = () => {
   return <GenerateLinkForm />;
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default GenerateLink;
