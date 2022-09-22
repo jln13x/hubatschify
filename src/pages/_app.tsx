d1cfdb0164cffb88364d1e30cef1460ec6dd995e
Julian <85513960+JLN13X@users.noreply.github.com>
2022-09-22 22:24:41 +0200

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,17 +8,24 @@ import type { AppType } from "next/app";
 import type { AppRouter } from "../server/router";
 import type { Session } from "next-auth";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <main className="grid h-screen w-screen place-items-center bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900">
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </main>
+    <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <main className="grid h-screen w-screen place-items-center bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900">
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </main>
+    </>
   );
 };
 
